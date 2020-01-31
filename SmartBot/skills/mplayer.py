7e0977ae9f2fab7e9067149d7e8378d5c678b200
Tom McCallum <this_is_not_an_email@no_where.com>
2020-01-31 15:50:31 +0000

    Modified mplayer skill to take a configuration file

--- SmartBot/skills/mplayer.py
+++ SmartBot/skills/mplayer.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import signal
+import json
 
 from config_io import DEFAULT_CONFIG_LOCATION
 from fifo import make_fifo
@@ -10,26 +11,28 @@ class MPlayer:
     STATE_PAUSED=0
     STATE_PLAYING=1
     STATE_STOPPED=2
+    config = {}
+
 
     def __init__(self):
+        config = json.load("conf/mplayer.json")
         self.state = MPlayer.STATE_STOPPED
         self.filename="mplayer.fifo"
         self.fifo = make_fifo(self.filename)
         self.track = None
 
     def pause_or_play(self):
-        if self.state == MPlayer.STATE_PLAYING or
-            self.state == MPlayer.STATE_PAUSED:
+        if self.state == MPlayer.STATE_PLAYING:
+            self._send_command_to(self.fifo, MPlayer.config["commands"]["pause"])
+            self.state = MPlayer.STATE_PAUSED
+        else:
             # note that for later versions of mplayer this changes to 'cycle pause'
             # so we need to handle the version of mplayer
-            self._send_command_to(self.fifo, "pause")
-            self.state = MPlayer.STATE_PAUSED
-        elif self.state == MPlayer.STATE_STOPPED:
             self.play()
 
     def play(self, track=None):
         if self.state == MPlayer.STATE_PAUSED:
-            self._send_command_to(self.fifo, "pause")
+            self._send_command_to(self.fifo, MPlayer.config["commands"]["continue"])
             self.state = MPlayer.STATE_PLAYING
         elif self.state == MPlayer.STATE_STOPPED:
             self.start(track)
@@ -59,8 +62,10 @@ class MPlayer:
         if track:
             self.track = track
         if self.track:
-            # @todo locate mplayer on system directly
-            self.process = subprocess.Popen("exec /usr/bin/mplayer --input-file \""+self.fifo+"\" --framedrop decoder -vo null \"" +self.track+"\"", shell=True, stdout=subprocess.PIPE)
+            shell_cmd = MPlayer.config["shell"]
+            shell_cmd = shell_cmd.replace("%fifo%", self.fifo)
+            shell_cmd = shell_cmd.replace("%track%", self.track)
+            self.process = subprocess.Popen("exec "+shell_cmd, shell=True, stdout=subprocess.PIPE)
             print("PID of mplayer process:"+str(self.process.pid))
             self.state = MPlayer.STATE_PLAYING
 
