ea83051e3dbf9208b712ec0f37461a752eee6112
Tom McCallum <this_is_not_an_email@no_where.com>
2020-06-23 01:24:03 +0100

    trying to unblock io thread

--- client/src/keyboard_thread.py
+++ client/src/keyboard_thread.py
@@ -67,9 +67,10 @@ class KeyboardListener(threading.Thread):
                 # tty.setraw(sys.stdin.fileno())
                 tty.setcbreak(sys.stdin.fileno())
                 #logging.debug("waiting for 3 bytes")
-                key = sys.stdin.read(1) # blocks until reads 3 bytes, which is correct for arrow keys, will detect CTRL+C
-                if key == chr(27):
-                    key += sys.stdin.read(2)
+                if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+                    key = sys.stdin.read(1) # blocks until reads 3 bytes, which is correct for arrow keys, will detect CTRL+C
+                    if key == chr(27):
+                        key += sys.stdin.read(2)
                 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
                 #logging.debug("handling key")
                 if key:
