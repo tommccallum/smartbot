817f422ae7d4b8cd4548374238ae6276713ea570
Tom McCallum <this_is_not_an_email@no_where.com>
2020-06-23 14:01:05 +0100

    refactoring of scripts

--- client/src/states/speaking_clock.py
+++ client/src/states/speaking_clock.py
@@ -37,7 +37,7 @@ class SpeakingClockState(ContinuousState):
         when = datetime.datetime.now()
         logging.debug("minute={} last_time={}".format(when.minute, self.last_date_said))
         if self.status == ContinuousState.RESUMING or self.last_date_said is None or when.strftime("%Y-%m-%d %H:%M") != self.last_date_said.strftime("%Y-%m-%d %H:%M"): # dont say the same datetime twice
-            if self.status == ContinuousState.RESUMING or when.minute == 0 or is_first_run: # ensure we say immediately when first loaded
+            if self.status == ContinuousState.RESUMING or when.second < 10 or is_first_run: # ensure we say immediately when first loaded
                 logging.debug("time is now: {}".format(when))
                 self.tell(when)
                 self.last_date_said = when # cache this time
