0a5a1eaf5107f5ed3378fa7f0377964ed8cf2942
Tom McCallum <this_is_not_an_email@no_where.com>
2020-06-30 23:20:24 +0100

    fixing bugs in sleep where end time is next day

--- client/src/personality.py
+++ client/src/personality.py
@@ -141,6 +141,7 @@ class Personality:
                                                 "%Y-%m-%d %H:%M")
         if end_time < soft_sleep:
             end_time = end_time + datetime.timedelta(days=1)
+        logging.debug("sleep {} {} {}".format(soft_sleep, hard_sleep, end_time))
         if globalvars.app_context and globalvars.app_context.is_asleep():
             if when > end_time:
                 ev = Event(EventEnum.EXIT_SLEEP)
